b97a74c24cf2fd51e5db22c61e206cc4063fb1cf
arjun123 <arjun@gmail.com>
2024-08-13 13:57:06 +0530

    Initial commit for CI/CD demo project

---
+++ src/test/java/com/example/ci_cd/_demo/CiCdDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ci_cd._demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCdDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
